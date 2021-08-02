45dc95cbb7eafbea51b84f2791634a58ac4b1223
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-08-02 12:43:00 -0400

    Removed the props from past logs

--- src/components/PastLogs.js
+++ src/components/PastLogs.js
@@ -1,11 +1,9 @@
 import {Link} from 'react-router-dom'
 import Button from './Button'
 
-const PastLogs = ({title, subtitle}) => {
+const PastLogs = () => {
     return (
         <div>
-            {/* <h1>{title}</h1>
-            <h2>{subtitle}</h2> */}
             <Link to='/'><Button text={'Go Back'}/></Link>
         </div>
     )
